951222c40037d82862625452368900db176a0ae9
Shreyas Rane <55063881+twopiaresquared@users.noreply.github.com>
2019-12-22 10:10:13 -0500

    Modulation for Deposit

--- DepositThread.java
+++ DepositThread.java
@@ -1,16 +1,19 @@
-public class DepositThread implements Runnable {
+public class DepositThread implements Runnable 
+{
    private Account account;
    private double amount;
 
-   public DepositThread(Account account, double amount) {
+   public DepositThread(Account account, double amount) 
+   {
 
-       // Set the account & balance
+       
        this.account=account;
        this.amount=amount;
    }
 
-   public void run() {
-       // make a deposit
+   public void run() 
+   {
+       
       
        account.deposit(amount);
        System.out.println("After Deposit:"+account.getBalance());
