bb6d093a188df845162c9b28b7b417190275821e
Shreyas Rane <55063881+twopiaresquared@users.noreply.github.com>
2019-12-22 10:12:07 -0500

    Final Changes to Withdraw

--- WithdrawThread.java
+++ WithdrawThread.java
@@ -1,17 +1,19 @@
-public class WithdrawThread implements Runnable {
+public class WithdrawThread implements Runnable 
+{
    private Account account;
    private double amount;
 
-   public WithdrawThread(Account account, double amount) {
+   public WithdrawThread(Account account, double amount) 
+   {
 
-       // Set the account & balance
+      
        this.account=account;
        this.amount=amount;
 
    }
 
    public void run() {
-       // make a withdrawal
+       
       
       
        account.withdraw(amount);
